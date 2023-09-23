48724c4117c4bf09ea88d80d72f52bde4893b3cd
franciwis <you@example.com>
2023-09-23 13:16:53 -0300

    1.5

---
+++ mychart.js
@@ -0,0 +1,41 @@
+import {renderDataF} from "./app.js";
+ 
+
+export const grafico = document.getElementById('myChartF').getContext("2d");
+
+const temperaturaHora = JSON.parse(localStorage.getItem("temperaturaHora"));
+const diaHora = JSON.parse(localStorage.getItem("diaHora"));
+// let diaHora = [1,3,2,4];
+// let temperatura = [2,1,3,4];
+
+
+
+const color1 = "blue";
+const color2 = "red";
+const color3 = "DarkCyan";
+const color4 = "OliveDrab";
+
+new Chart(grafico, {
+
+  type: 'line',
+  data: {
+    labels: diaHora,
+    datasets: [{
+      label: 'Pronóstico del clima',
+      data: temperaturaHora,
+      borderWidth: 1,
+      backgroundColor:color3,
+      borderColor:color4
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+
+
