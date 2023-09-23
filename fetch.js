505a22a1a367f1dc8cee159f30c8842585d38d5f
franciwis <you@example.com>
2023-09-23 13:13:44 -0300

    1.3

---
+++ fetch.js
@@ -0,0 +1,40 @@
+export const ApiClima1 = async(url) => {
+    try{
+        const respuesta = await fetch (url);
+        
+        // console.log(respuesta);
+
+        const datos = await respuesta.json();
+
+
+    return datos;
+
+    }catch(error){
+        console.log(error)
+    }
+}
+
+
+export const fetchApi = async (url) => {
+    const response = await fetch(url);
+    const data = response.json();
+    return data;
+}
+
+
+export const ApiTempActual = async(url) => {
+    try{
+        const respuesta = await fetch (url);
+        
+        // console.log(respuesta);
+
+        const datos = await respuesta.json();
+
+
+    return datos;
+
+    }catch(error){
+        console.log(error)
+    }
+}
+// ApiClima1();
